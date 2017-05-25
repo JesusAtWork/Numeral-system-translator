3c60b76c92c0335c2f221ffbdc0c8fa45e888076
JesusAtWork <carloscor8989@gmail.com>
2017-05-24 22:53:58 -0400

    clean

--- Translator.py
+++ Translator.py
@@ -3,19 +3,18 @@ Dec="0123456789"
 Hex="0123456789ABCDEF"
 Oct="01234567"
 Bin="01"
-def getD(j,k):
+def getD(j,k):    #Return to decimal
     n=0
     for i in range(len(j)):
        n+=int(k.find(str(j[len(j)-i-1])))*(pow(len(k),i))
-    return(n)   #Return as Number
+    return(n)
 
-def setD(j,k):  
+def setD(j,k):    #Convert from decimal
     l=int(j)
     n=""
     while(l>0):
        n=k[l%len(k)]+n
        l=math.floor(l/len(k))
-    return(n)   #Return as String
+    return(n)
 
-print(setD("40",Bin))
-print(getD("FF",Hex))
+print(setD(getD("10110101",Bin),Hex))
